390edecfd5c5027fcbf230223ec198bb44596417
Claire Felt <cfeltgungatan@gmail.com>
2024-10-06 00:23:59 -0600

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,33 @@ btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open');
     navElement.classList.toggle('open');
 })
+
+//grab the REVIEWS data from the file
+import { reviews } from '../data/reviews.js';
+console.log(reviews);
+
+//create an empty card
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < reviews.length; x++) {
+
+    //create a section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+    const myReview = document.createElement('p')
+    myReview.textContent = reviews[x].text
+
+    //build the figure with child elements
+    mySection.appendChild(myName);
+    mySection.appendChild(myReview);
+
+    //Add a new card to the page
+    myTarget.appendChild(mySection);
+
+}
+
+
 //Put current year in footer
 const theYear = new Date();
 document.querySelector('#year').textContent = theYear.getFullYear()
