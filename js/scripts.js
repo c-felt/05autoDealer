b4f363ee7522053a119e354f605f86b20c911952
Claire Felt <cfeltgungatan@gmail.com>
2024-10-14 20:22:59 -0600

    trying to get the stars to work

--- js/scripts.js
+++ js/scripts.js
@@ -26,12 +26,12 @@ const myRatings = document.createElement('div')
 
 for (let step = 0; step<reviews[x].stars; step++) {
     const starIndicator = document.createElement('img')
-    starIndicator.src = "./images/starGraphic.svg"
+    starIndicator.src = "./images/starGraphic.svg";
     myRatings.appendChild(starIndicator)
 }
     //build the figure with child elements
     mySection.appendChild(myName);
-    mySection.appendChild(myRatings)
+    mySection.appendChild(myRatings);
     mySection.appendChild(myReview);
 
     //Add a new card to the page
